c5479f356ef73d28a24ea7bb1e685e5037215abc
Magdalena Nowowiejska <47540038+ArchyArch@users.noreply.github.com>
2019-10-14 21:06:51 +0200

    Removed unnecessary content.

--- messenger-link-exporter.py
+++ messenger-link-exporter.py
@@ -7,7 +7,6 @@ messages = conversation['messages']
 sender_messages = list(filter(lambda x: x['sender_name'] == conversation['participants'][0]['name'], messages))
 sender_messages_filtered = list(filter(lambda x: True if 'content' in x else False, messages))
 messages_content = list(map(lambda x: x['content'], sender_messages_filtered))
-print(messages_content[0])
 list_of_url = list(map(lambda x: re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', x), messages_content))
 list_of_url_without_empty_lists = list(filter(lambda x: x != [], list_of_url))
 

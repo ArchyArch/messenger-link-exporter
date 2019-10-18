8182944e30a2a9fda96dd9660ec629ecfd45e8a5
Magdalena Nowowiejska <nowowiejska.magda@gmail.com>
2019-10-18 21:50:22 +0200

    Handle invalid user input

--- messenger-link-exporter.py
+++ messenger-link-exporter.py
@@ -1,6 +1,21 @@
 import json
 import re
 from sys import argv
+from sys import exit
+
+def show_participant_selection_menu(conversation):
+    participants = conversation['participants']
+    participant_number = 0
+
+    for participant in participants:
+        print(f'{participant_number}. {participant["name"]}')
+        participant_number = participant_number + 1
+    print("E. Everyone")
+
+    print("Choose option:")
+    choice = input()
+
+    return [choice, participants]
 
 path_to_json_file = argv.pop()
 
@@ -12,17 +27,30 @@ except IndexError:
 f = open(path_to_json_file, 'r')
 conversation = json.load(f)
 messages = conversation['messages']
-sender_messages = list(filter(lambda x: x['sender_name'] == conversation['participants'][0]['name'], messages))
-sender_messages_filtered = list(filter(lambda x: True if 'content' in x else False, messages))
-messages_content = list(map(lambda x: x['content'], sender_messages_filtered))
-list_of_url = list(map(lambda x: re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', x), messages_content))
-list_of_url_without_empty_lists = list(filter(lambda x: x != [], list_of_url))
 
-flat_list = [item for sublist in list_of_url_without_empty_lists for item in sublist]
-list_of_links = open("list_of_links.txt", 'w')
+while True:
+    choice, participants = show_participant_selection_menu(conversation)
+    try:
+        if choice == "e" or choice == "E":
+            sender_messages_filtered = list(filter(lambda x: True if 'content' in x else False, messages))
+        else:
+            choice = int(choice)
+            participant_name = participants[choice]['name']
+            sender_messages = list(filter(lambda x: x['sender_name'] == participant_name, messages))
+            sender_messages_filtered = list(filter(lambda x: True if 'content' in x else False, sender_messages))
 
-for item in flat_list:
-    list_of_links.write(item)
-    list_of_links.write("\n")
+        messages_content = list(map(lambda x: x['content'], sender_messages_filtered))
+        list_of_url = list(map(lambda x: re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', x), messages_content))
+        list_of_url_without_empty_lists = list(filter(lambda x: x != [], list_of_url))
 
-list_of_links.close()
+        flat_list = [item for sublist in list_of_url_without_empty_lists for item in sublist]
+        list_of_links = open("list_of_links.txt", 'w')
+
+        for item in flat_list:
+            list_of_links.write(item)
+            list_of_links.write("\n")
+
+        list_of_links.close()
+        exit()
+    except ValueError:
+        print("Choose a valid option!")
